e9053f72c37595f13970466d9d02879d01071776
Alexander Blochel <alexanderblochel@ufl.edu>
2026-02-05 13:06:00 -0500

    added predator prey model

--- simulating data.R
+++ simulating data.R
@@ -1,16 +1,17 @@
 
 # simulating data ---------------------------------------------------------
 
+library('deSolve')
 library('ggplot2')
 
 
 
-# water depths 
+
+# Water Depths  -----------------------------------------------------------
 
 
-
-mean_depth = 30
-amplitude = 30
+mean_depth = 40
+amplitude = 40
 maxday = 150
 sd = 0
 
@@ -59,6 +60,29 @@ water_year |>
   theme(text = element_text(size = 20)) 
 
 
+
+
+# Lotka Volterra - model --------------------------------------------------
+
+
+
+
+LotVmod <- function (Time, State, Pars) {
+  with(as.list(c(State, Pars)), {
+    dx = x*(alpha - beta*y)
+    dy = -y*(gamma - delta*x)
+    return(list(c(dx, dy)))
+  })
+}
+
+Pars <- c(alpha = 2, beta = .5, gamma = .2, delta = .6)
+State <- c(x = 10, y = 10)
+Time <- seq(0, 100, by = 1)
+
+out <- as.data.frame(ode(func = LotVmod, y = State, parms = Pars, times = Time))
+
+matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
+legend("topright", c("Cute bunnies", "Rabid foxes"), lty = c(1,2), col = c(1,2), box.lwd = 0)
 
 
 
