a6aa7f0d5ff4f62a1dce30258058d5d35e6db574
Alexander Blochel <alexanderblochel@ufl.edu>
2026-02-11 09:25:09 -0500

    lorenz attractor

--- simulating data.R
+++ simulating data.R
@@ -185,9 +185,30 @@ PrPred(3, 0.5, 2, 2)
 
 
 
+# Lorenz Attractor Equations --------------------------------------------------------
 
 
+parameters <- c(s = 10, r = 28, b = 8/3)
+state <- c(X = 0, Y = 1, Z = 1)
 
+Lorenz <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    dX <- s * (Y - X)
+    dY <- X * (r - Z) - Y
+    dZ <- X * Y - b * Z
+    list(c(dX, dY, dZ))
+  })
+}
+
+
+times <- seq(0, 50, by = 0.01)
+
+out <- ode(y = state, times = times, func = Lorenz, parms = parameters)
+
+par(oma = c(0, 0, 3, 0))
+plot(out, xlab = "time", ylab = "-")
+plot(out[, "Y"], out[, "Z"], pch = ".", type = "l")
+mtext(outer = TRUE, side = 3, "Lorenz model", cex = 1.5)
 
 
 
@@ -205,12 +226,6 @@ PrPred(3, 0.5, 2, 2)
 
 #dynamic forage model, water dynamics and pred prey interactions 
 # wetness? 
-
-
-
-
-
-
 
 
 
