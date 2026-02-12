20fe8b6b284148ff0b185c761c3a90654e8ad755
Alexander Blochel <alexanderblochel@ufl.edu>
2026-02-12 14:29:59 -0500

    info on time to event forecasting

---
+++ time_to_event_forecasting.R
@@ -0,0 +1,29 @@
+
+# time to event forcasting ------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+# https://medium.com/@lahsaini/time-to-event-survival-analysis-6bbd4ce4e8bf
+
+# https://www.statforbiology.com/_seedtutorial/time-to-event-models-for-seed-germinationemergence
+
+# https://doseresponse.github.io/medrc/articles/medrc.html
+
+# https://www.statforbiology.com/2026/stat_drcte_6-ht1step/ or https://www.r-bloggers.com/2026/02/fitting-time-to-event-models-with-an-environmental-covariate/
+
+
+
+
+
+
+
